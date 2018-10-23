0003a42b2d5698744e34c63f9a5586fadcb80bf2
Eric Weissman <eric.weissman21@gmail.com>
2018-10-23 16:12:02 -0600

    Add correct event listener on delete image

--- idea.js
+++ idea.js
@@ -1,20 +1,22 @@
 class Idea {
   constructor(title, body){
+    this.id = 'Idea: ' + Date.now();
     this.title = title;
     this.body = body;
+    this.quality = 'Swill';
   }
 
 
   saveToStorage(title, body){
-    var num = new Date();
-    num += title;
+    // var num = new Date();
+    // num += title;
 
-    localStorage.setItem(num, body);
+    localStorage.setItem(this.id, body);
 
   };
 
-  deleteFromStorage(){
-
+  deleteFromStorage(title){
+    localStorage.removeItem(title);
   };
 
   updateSelf(){
