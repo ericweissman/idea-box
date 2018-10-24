5244982e1ce7c6ad36ddf415a466b7d0a4b32dcc
Travis Gee <geet084@gmail.com>
2018-10-24 16:53:33 -0600

    Move card info into Idea class

--- idea.js
+++ idea.js
@@ -6,6 +6,23 @@ class Idea {
     this.quality = 'Swill';
   }
 
+  cardInfo(ideaCard) {
+    ideaCard.innerHTML = 
+      `<section class="card-title-body">
+            <h2 class="card-title-text">${this.title}</h2>
+            <p class="card-body-text">${this.body}</p>
+          </section>
+          <section class="card-actions" id= ${this.id} data-name="${this.id}"> 
+            <div class="action-btns">
+              <button class="downvote"><img class="downvote-img"src="images/downvote.svg" alt="down button"></button>
+              <button class="upvote"><img class="upvote-img"src="images/upvote.svg" alt="up button"></button>
+              <h3 class="idea-quality">QUALITY: Swill</h3>
+            </div>
+            <img class="delete" src="images/delete.svg" alt="delete button">
+          </section>    
+      `;
+  }
+
 
   saveToStorage(){
     localStorage.setItem(this.id, JSON.stringify(this));
