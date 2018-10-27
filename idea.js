2e4c5f389cf8a38e37a9d6dc8f7c09a23e90858c
Travis Gee <geet084@gmail.com>
2018-10-27 15:37:39 -0600

    Finish quality functionality and start editing process

--- idea.js
+++ idea.js
@@ -9,14 +9,14 @@ class Idea {
   cardInfo(ideaCard) {
     ideaCard.innerHTML = 
       `<section class="card-title-body">
-            <h2 class="card-title-text">${this.title}</h2>
-            <p class="card-body-text">${this.body}</p>
+            <h2 class="card-title-text" contenteditable="true">${this.title}</h2>
+            <p class="card-body-text" contenteditable="true">${this.body}</p>
           </section>
           <section class="card-actions" id= ${this.id} data-name="${this.id}"> 
             <div class="action-btns">
               <img class="downvote-img"src="images/downvote.svg" alt="down button">
               <img class="upvote-img"src="images/upvote.svg" alt="up button">
-              <h3 class="idea-quality">QUALITY: Swill</h3>
+              <h3 class="idea-quality">QUALITY: ${this.quality}</h3>
             </div>
             <img class="delete" src="images/delete.svg" alt="delete button">
           </section>    
@@ -32,11 +32,14 @@ class Idea {
     localStorage.removeItem(event.target.parentElement.id)  
   };
 
-  updateSelf(){
-
+  updateSelf(updatedTitle){
+    this.title = updatedTitle;
+    // this.body = updatedBody;
+    return this.title;
   }
 
-  updateQuality(){
-  //  THIS WILL BE OUR NEXT STEP
+  updateQuality(qual){
+    this.quality = qual;
+    return this.quality;
   }
 }
